533025aa8bd5bf49f08da6fb2384f448c2f9029b
prasadtanakala <159104522+prasadtanakala@users.noreply.github.com>
2026-06-26 08:25:34 +0000

    add css

--- chat-app/src/app/components/login/login.ts
+++ chat-app/src/app/components/login/login.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule, MatLabel } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [MatInputModule,MatCardModule,MatLabel,MatFormFieldModule],
+  imports: [MatInputModule,MatCardModule,MatLabel,MatFormFieldModule,MatIconModule,MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
